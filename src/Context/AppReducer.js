01c1f4dcdb8054329348554412a12fc6c183a3f4
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2023-02-16 12:28:49 +0400

    first commit

--- src/Context/AppReducer.js
+++ src/Context/AppReducer.js
@@ -1,3 +1,13 @@
-export default (state,action) =>{
-    
+export  default AppReducer = (state, action) => {
+
+    switch (action.type) {
+        
+        case "ADD_MOVIE_TO_WATCHLIST":
+        return {
+            ...state,
+            watchlist: [...state.watchlist, action.payload],
+        };
+        default:
+        return state;
+    }
 }
