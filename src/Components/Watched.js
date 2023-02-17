bc0f0499a56078685d1d4d378bc0866747789d73
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2023-02-17 16:58:00 +0400

    Movie_Project was Ready problem fixed

--- src/Components/Watched.js
+++ src/Components/Watched.js
@@ -17,7 +17,7 @@ const Watched = () => {
         {watched.length > 0 ? (
           <div className="movie-grid">
             {watched.map((movie) => (
-              <MovieCard movie={movie} type="watched" />
+              <MovieCard movie={movie} type="watched" key={movie.id} />
             ))}
           </div>
         ) : (
